c6b474f3298b78efb8df6908aacc52ae483a38c0
Arthur05 <arthurfotso.bfkg@gmail.com>
2025-07-10 00:22:47 +0200

    reference(Home page): adding news informations in the Hero section (Social Network links)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import HeroSection from "@/components/ui/HeroSection";
 import React from "react";
 
 const Home = () => {
@@ -13,19 +7,7 @@ const Home = () => {
     <div>
       <Card className="flex w-fit">
         <CardHeader className="w-[10rem]">
-          <div className="w-[10rem] flex rounded-full border-4 border-blue-600 h-[10rem] bg-[url(/profile.png)] bg-center bg-cover"></div>
-          <CardTitle className="flex flex-col items-center gap-2 text-start mt-[0.9rem]">
-            <span className="w-full">Arthur Jordy</span>
-            <span className="w-full text-sm text-slate-400">
-              Fullstack developer
-            </span>
-          </CardTitle>
-          <Button className="bg-blue-600 cursor-pointer hover:bg-blue-700 transition duration-100">
-            arthurfotso.bfkg@gmail.com
-          </Button>
-          <CardDescription className="mt-[1rem]">
-            <span className="text-xs">20 ans - Cameroun, Bafoussam</span>
-          </CardDescription>
+          <HeroSection />
         </CardHeader>
         <CardContent></CardContent>
       </Card>
